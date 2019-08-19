a0538053f92e289b1aec85559e04bc5a9f3efb96
Antonio Dias <addsjunior@gmail.com>
2019-08-19 08:39:35 -0300

    face detect

--- 5-face-detect/aula.py
+++ 5-face-detect/aula.py
@@ -4,15 +4,28 @@ import sys
 caminho = sys.argv[1]
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-img = cv2.imread(caminho)
+original = cv2.imread(caminho)
+img = cv2.resize(
+  original, 
+  (int(original.shape[1]/3), int(original.shape[0]/3))
+)
+
 cinza = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 faces = faceCascade.detectMultiScale(
   cinza,
-  scaleFactor=1.1,
+  scaleFactor=1.07,
   minNeighbors=5,
-  minSize=(50, 50),
+  minSize=(20, 20),
   flags=cv2.CASCADE_SCALE_IMAGE
 )
 
 print("Faces encontradas:", len(faces))
+
+for (x, y, w, h) in faces:
+  cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+# cv2.imshow("Faces", img)
+# cv2.waitKey(0)
+cv2.imwrite("faces-" + caminho, img)
+cv2.destroyAllWindows()
